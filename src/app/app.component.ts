2320bdb8234561192e2c050ef0a9210cf225c125
Sandra Ruiz Jimenez (ext) <sandra.ruiz.external@atos.net>
2023-04-17 15:37:47 +0200

    Subiendo ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'calculator';
+  title = 'Mini Calculadora';
+
+  number1: number = 0;
+  number2: number = 0;
+  result: number = 0;
+  error: string = '';
+
+  addition(): void {
+    this.result = this.number1 + this.number2;
+    this.number1 = 0;
+    this.number2 = 0;
+  }
+
+  substraction(): void {
+    this.result = this.number1 - this.number2;
+    this.error = '';
+    this.number1 = 0;
+    this.number2 = 0;
+  }
+  
+  division(): void {
+    
+    if(this.number2!=0) {
+      this.result = this.number1 / this.number2;
+      this.error = '';
+    }else {
+      this.result = 0;
+      this.error = "No se puede dividir entre 0"
+    }
+    this.number1 = 0;
+    this.number2 = 0;
+  }
+
+  multiplication(): void {
+    this.result = this.number1 * this.number2;
+    this.number1 = 0;
+    this.number2 = 0;
+    this.error = '';
+  }
 }
